aafae48c8f397c6f1ce84657c86e320ccede3047
Kaanki <kaan_9435@hotmail.com>
2023-01-09 21:25:08 +0300

    Creating Project Base

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")] //localhost:5000/Controller
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
