b9e09bcc6a6493aa0b4e17c3b0f2c9a5d2e0adb1
Kaanki <kaan_9435@hotmail.com>
2023-01-12 22:29:25 +0300

    implement AutoMapper and Delete operations

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -5,30 +5,25 @@ using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities;
-public class Edit
+public class Delete
 {
 
     public class Command : IRequest
     {
-        public Activity Activity { get; set; }
+        public Guid Guid { get; set; }
     }
 
     public class Handler : IRequestHandler<Command>
     {
         private readonly DataContext _context;
-        public IMapper _mapper { get; }
-        public Handler(DataContext context, IMapper mapper)
+        public Handler(DataContext context)
         {
             this._context = context;
-            _mapper = mapper;
         }
-
-
-
         public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
         {
-            var activity = await _context.Activities.FindAsync(request.Activity.Id);
-            _mapper.Map(request.Activity, activity);
+            var activity = await _context.Activities.FindAsync(request.Guid);
+            _context.Remove(activity);
 
             await _context.SaveChangesAsync();
 
